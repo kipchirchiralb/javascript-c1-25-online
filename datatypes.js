1fe270800ede2c89011c05a9b7cb26b516e9acfe
Eldohub Academy <academy@eldohub.co.ke>
2025-03-05 15:25:04 +0300

    datatypes and variables

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+// We can store/manipulate/use different types of data in js programs
+//These include:- number, string, boolean, null, undefined, object, array
+let age = 30 // number
+let rate = 3.25 // number
+let email = "alber@324342$.com" // string "" ''
+// boolean - true or flase
+let isKenyan = true
+let isLoggedIn = false
+// null - intentional absence of a value
+let score = null
+// udefined -- you have assigned a value to a variable
+let username 
+console.log( username );
+console.log(age);
+// object -- properties and methods of related data
+let user = {
+    name: "kevin",
+    age: 40,
+    gender: "male",
+    siblings: ["jane", "vincent"],
+    borrowBook: ()=>{
+        console.log("Borrowing a book");
+        
+    }
+}
+console.log(user);
+user.borrowBook()
+// arrays -- list of data []
+const colors = ["red","green","yellow"]
+const data = ["3434", 35.43, true, "again", null ]
+// json 
+// kipchirchialb -- j c125 online
---
+++ variables.js
@@ -0,0 +1,20 @@
+// Varaiables allow for storing data in memory as your program/app/code is running
+// In javascript we create variables using const, let and var keywords
+
+const email = "albert@eldohub.co.ke" // constant variable- it will not change at any point as the application is running
+
+let score = 10 // this variable, the value can be changed/modified/reassigned as the app is running
+
+// var was used before es6(2015) to create variables before let and const were introduced. 
+var age = 60
+
+// Variables can store data of any type(number,string, boolean...)
+
+// keyword varablename = varablevalue
+score = 15
+
+email = "kipchirchir@eldohub.co.ke"
+
+// Variable naming rules and variable naming conventions in js--- Assignment
+
+// The truth table  ---  logical tool -- computers 
